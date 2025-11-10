ab24c427fdc7d7eb0cfbebbf684cdfa489acde1f
Milan Marcinčo <mmsprava@gmail.com>
2025-11-10 14:34:22 +0100

    Clean up

--- components/helpers/helpers.c
+++ components/helpers/helpers.c
@@ -10,23 +10,3 @@ static void get_mac_address_string(char *mac_str) {
 	ESP_ERROR_CHECK(esp_wifi_get_mac(ESP_IF_WIFI_STA, mac));
 	snprintf(mac_str, MAC_LEN, "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 }
-
-char *dynamic_format(const char *fmt, ...) {
-	va_list args;
-	va_start(args, fmt);
-	int len = vsnprintf(NULL, 0, fmt, args);
-	va_end(args);
-
-	if (len < 0)
-		return NULL;
-
-	char *buffer = malloc(len + 1);
-	if (!buffer)
-		return NULL;
-
-	va_start(args, fmt);
-	vsnprintf(buffer, len + 1, fmt, args);
-	va_end(args);
-
-	return buffer;
-}
--- components/helpers/include/helpers.h
+++ components/helpers/include/helpers.h
@@ -26,5 +26,3 @@
 	} while (0)
 
 #define MAC_LEN 18
-
-char *dynamic_format(const char *fmt, ...);
